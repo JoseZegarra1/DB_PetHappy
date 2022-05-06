2fceb8c649e20c6402bfb1fc13adc8c40ec36ced
JoseZegarra1 <jose.zegarra@vallegrande.edu.pe>
2022-05-06 14:52:09 -0500

    Registros de DBPetHappy

---
+++ Data/dbo.PRODUCTO_Data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[PRODUCTO] ON
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (1, N'CAMA PELITOS', N'Dayli Stuff', 20.00, N'Hecho de felpa larga, súper suave y súper calido', N'A', N'SKU')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (2, N'TRANSPORTADOR', N'Transportadora route 2', 50.00, N'Transportador de viaje de plástico para mascotas', N'A', N'BREYK')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (3, N'LECHE EN POLVO', N'BRIT CARE ADULTO', 30.00, N'Brit Care Adult está adecuadamente balanceado', N'A', N'BRIT')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (4, N'LECHE EN POLVO', N'BRIT MONO PROTEIN', 40.00, N'Es un alimento húmedo indicado para todos los perros', N'A', N'BRIT')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (5, N'LECHE EN POLVO', N'BRIT CARE PATE', 20.00, N'Es un alimento húmedo completo indicado para perros adultos', N'A', N'BRIT')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (6, N'LECHE EN POLVO', N'BRIT CARE PATE & MEAT', 47.00, N'Formulado con 70% de pollo y salmón + paté puro de carne', N'A', N'BRIT')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (7, N'CROQUETAS', N'ROYAL CANIN MAXI', 78.00, N'Es un alimento húmedo completo', N'A', N'ROYAL')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (8, N'HIGIENE & GROOMING', N'ADIESTRADOR DE CACHORROS', 14.00, N'Ayuda al cachorro a hacer sus necesidades', N'A', N'TRAINER')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (9, N'HIGIENE & GROOMING', N'AFP DOG SPARKLE SET', 17.00, N'Kit Dental tiene todo lo que se necesita', N'A', N'AFP DOG')
+INSERT INTO [dbo].[PRODUCTO] ([IDPRO], [TIPPRO], [NOMPRO], [PREPRO], [DESPRO], [ESTPRO], [MARCPRO]) VALUES (10, N'HIGIENE & GROOMING', N'BOLSAS REPUESTO DISPENSADOR', 84.00, N'Es el repuesto perfecto para su Kit Klint', N'A', N'HAGEN')
+SET IDENTITY_INSERT [dbo].[PRODUCTO] OFF
